c2208ae92729548ac8308aa98d1317fb3dcf4a1f
xsdbool <72165128+xsdbool@users.noreply.github.com>
2023-11-07 17:53:01 +0100

    Patch 1 (#2)
    * patch-1
    
    * patch-1
    
    ---------
    
    Co-authored-by: xsdbool <pederm9ller@gmail.com>

---
+++ api/errors.go
@@ -0,0 +1,17 @@
+package api
+
+import "errors"
+
+var (
+	ErrSessionExpired   = errors.New("session expired or unauthorized")
+	ErrFunctionDisabled = errors.New("function currently disabled")
+	ErrBidding          = errors.New("error bidding on item")
+	ErrListing          = errors.New("error listing item")
+	ErrWatchlist        = errors.New("error getting watchlist")
+	ErrClearWatchlist   = errors.New("error clearing watchlist")
+	ErrReslist          = errors.New("error reslisting items")
+	ErrClearSold        = errors.New("cannot clear sold items")
+	ErrHTTPRequest      = errors.New("HTTP request error")
+	ErrInvalidBidPrice  = errors.New("bid price is invalid")
+	ErrMoveItem         = errors.New("error putting Item to pile")
+)
